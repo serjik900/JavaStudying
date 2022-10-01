ee82f65464b2219a4dc141fbb84863bb9367fa7e
serjik900 <serhiimedia@gmail.com>
2022-10-01 18:03:27 -0500

    First HW

---
+++ src/Class14/HW1.java
@@ -0,0 +1,22 @@
+package Class14;
+
+public class HW1 {
+
+    void largestNumber(int num1,int num2){
+        if (num1 > num2) {
+            System.out.println(num1 + " is Larrgest number");
+
+        } else {
+            System.out.println(num2 + " is Largest number");
+        }
+    }
+
+    public static void main(String[] args) {
+       HW1 obj=new HW1();
+            obj.largestNumber(2,4);
+/*
+
+    }}
+
+
+
