13c68157e34321fde2bd15cadbed1b1bdeef9fe5
serjik900 <serhiimedia@gmail.com>
2022-10-22 12:55:36 -0500

    Group Project2

---
+++ src/GProject2/Shape.java
@@ -0,0 +1,25 @@
+package GProject2;
+
+
+public interface Shape {
+    void calculateArea(double length);
+    void calculatePerimeter(double length);
+}
+
+class Circle implements Shape{
+
+    @Override
+    public void calculateArea(double radius) {
+        System.out.println(radius*radius*Math.PI); }
+    @Override
+    public void calculatePerimeter(double radius) {
+        System.out.println(2*radius*Math.PI); }
+}
+class Square implements Shape{
+    @Override
+    public void calculateArea(double length) {
+        System.out.println(length*length); }
+    @Override
+    public void calculatePerimeter(double length) {
+        System.out.println(4*length); }
+}
---
+++ src/GProject2/ShapeTester.java
@@ -0,0 +1,17 @@
+package GProject2;
+
+public class ShapeTester {
+    public static void main(String[] args) {
+
+        Circle circle =new Circle();
+        Square square = new Square();
+
+        circle.calculatePerimeter(5.8);
+        circle.calculateArea(5.8);
+
+        square.calculatePerimeter(6.2);
+        square.calculateArea(6.2);
+
+
+    }
+}
